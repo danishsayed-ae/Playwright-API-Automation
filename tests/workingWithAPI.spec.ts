17e8842bb8575a658f446444c67998b3c366a8f9
Danish Sayed <danishsayed.ae@gmail.com>
2024-06-07 06:47:34 +0400

    Added tags.json

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,20 +1,21 @@
 import { test, expect } from '@playwright/test';
+import tags from '../test-data/tags.json'
 
-test.beforeEach( async ({page}) => {
+test.beforeEach(async ({ page }) => {
   // To mock the API response we need to route it before we actually load the page
   // Using the "route" method we can mock the API and then using the callback function we can send the updated response to the API endpoint
-  await page.route('https://conduit-api.bondaracademy.com/api/tags', async route => {
-    const tags = {
-      "tags": [
-          "Automation",
-          "Testing"
-      ]
-  }
-  // We use the "fulfill" method to post the API response
-  await route.fulfill({
-    // We will be updating the body of our API and using "stringify" method to convert JSON to string
-    body: JSON.stringify(tags)
+  await page.route('*/**/api/tags', async route => {
+    // We use the "fulfill" method to post the API response
+    await route.fulfill({
+      // We will be updating the body of our API and using "stringify" method to convert JSON to string
+      body: JSON.stringify(tags)
+    })
   })
+
+  await page.route('*/**/api/articles?limit=10&offset=0', async route => {
+    const response = await route.fetch()
+    const responseBody = await response.json()
+
   })
 
   await page.goto('https://conduit.bondaracademy.com/')
